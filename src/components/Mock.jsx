b930d72a76fc56c3645a9c34305e429e6675a1f6
Ziyan.yeye <ziyan.yeye@bytedance.com>
2023-08-07 21:06:02 +0800

    fix mobile mock display

--- src/components/Mock.jsx
+++ src/components/Mock.jsx
@@ -6,20 +6,24 @@ import { FileInput } from "@/components/FileInput"
 // export const metadata = {
 
 // }
+
+const paddingRatio = 390 / 456;
+
 const Mock = ({ titleA, titleB, subtitle, overlay, setOverlay }) => {
     return (
         <div className='flex flex-col items-center'>
             <div>
-                <div className='w-full h-full p-24 flex items-center justify-center absolute'>
-                    <div style={{ width: 456 * 0.8, }}>
+                <div className='w-full h-full p-24 flex items-center justify-center absolute md:scale-150'>
+                    <div style={{ width: 456 * 0.5 }} className='absolute'>
                         <img src="/img/iPhone14-Midnight-Portrait.png" alt="My Photo" />
                     </div>
-                </div>
-                <div className='w-full h-full p-24 flex items-center justify-center absolute'>
-                    <div style={{ width: 390 * 0.8, }}>
+                    <div style={{ width: 390 * 0.5 }} className='absolute'>
                         <img src={overlay} alt="Overlay" />
                     </div>
                 </div>
+                {/* <div className='w-full h-full p-24 flex items-center justify-center absolute'>
+                    
+                </div> */}
             </div>
         </div>
 
