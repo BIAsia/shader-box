1bebc9cdb12032e59120a3d44d5fbbbd23c28d75
Ziyan.yeye <ziyan.yeye@bytedance.com>
2023-08-09 16:53:22 +0800

    md scale

--- src/components/Mock.jsx
+++ src/components/Mock.jsx
@@ -13,10 +13,10 @@ const Mock = ({ titleA, titleB, subtitle, overlay, setOverlay }) => {
     return (
         <div id='mock' className='w-full flex flex-col items-center justify-center'>
             <div className='w-screen h-screen flex items-center justify-center absolute top-0 left-0'>
-                <div style={{ width: 456 * 0.6 }} className='absolute'>
+                <div style={{ width: 456 * 0.6 }} className='absolute md:scale-125'>
                     <img src="/img/iPhone14-Midnight-Portrait.png" alt="My Photo" />
                 </div>
-                <div style={{ width: 390 * 0.6 }} className='absolute'>
+                <div style={{ width: 390 * 0.6 }} className='absolute md:scale-125'>
                     <img src={overlay} alt="Overlay" />
                 </div>
             </div>
