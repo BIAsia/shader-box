ecb821a3fa2468028f8efab9d286edba10f21ea8
lzybiasia <l980492289@163.com>
2024-07-02 16:06:31 +0800

    sharp gradient column parameter arrangement

--- src/templates/Shader/sharpGradientR/sharpGradientR.tsx
+++ src/templates/Shader/sharpGradientR/sharpGradientR.tsx
@@ -16,19 +16,18 @@ const SharpGradientRMaterial = shaderMaterial(
   {
     uResolution: new THREE.Vector2(0, 0),
     uTime: 0,
-    uSpeed: 0.05,
-    uNoiseDensity: 1.2,
-    uNoiseStrength: 1.4,
+    uSpeed: 1.,
+    uTimeOffset: 0.0,
+    uLightness: 0.,
+    uPosition: new THREE.Vector2(0.0, 0.0),
+    uScale: new THREE.Vector2(1.0, 1.0),
+    uRotate: 0.,
     uColor: ["#ef233c", "#8d99ae", "#2b2d42", "#000000"].map(
       (color) => new THREE.Color(color)
     ),
-    uLightness: 0.2,
-    uDensity: 25.,
-    uPosEffect: new THREE.Vector2(1., 0.5),
-    // uEffect: 0.9,
-    // uMorph: 1.54,
-    uDirection: new THREE.Vector2(1, 1),
-    uCol: 25,
+    uBgColor: new THREE.Color('#000000'),
+    uComplex: 1,
+    uMorph: 0.0,
   },
   vertex,
   fragment
@@ -55,34 +54,34 @@ const SharpGradientRBg = (props: Mesh) => {
     })
   });
   //const waterBgStore = useCreateStore();
-  const { scale, morph, effect, noisy } = useControls({
-    // scale: { value: 1.0, min: 0.1, max: 3 },
-    // morph: { value: 1.52, min: 0.2, max: 3.5 },
-    // effect: { value: { x: 1, y: 1 } },
-    noisy: true,
-  }, { storeId: 'water-gradient' });
-
-  const colors = useControls({
-    colors: folder({
-      color1: '#2461f0',
-      color2: '#5179be',
-      color3: '#2b2d42',
-      color4: '#000000',
-    })
-  }, { storeId: 'water-gradient' });
-
   const animation = useControls({
     animation: folder({
-      speed: { value: 3, min: 0.1, max: 3 },
+      speed: { value: 1, min: 0., max: 10 },
+      timeOffset: { value: 0, min: 0., max: 10 },
     }, { collapsed: false })
-  }, { storeId: 'water-gradient' });
+  });
 
-  const advanced = useControls({
-    advanced: folder({
-      columns: { value: 25, min: 1, max: 50 },
+  const color = useControls({
+    color: folder({
+      color1: '#ef233c',
+      color2: '#8d99ae',
+      color3: '#2b2d42',
+      color4: '#000000',
+      bgColor: '#000000',
       lightness: { value: 0., min: - 1, max: 1 },
-    }, { collapsed: false })
-  }, { storeId: 'water-gradient' });
+    })
+  });
+
+  const shape = useControls({
+    shape: folder({
+      position: { value: { x: 0, y: 0 }, step: 0.01 },
+      scaleX: { value: 1.0, min: 0.1, max: 10 },
+      scaleY: { value: 1.0, min: 0.1, max: 10 },
+      // rotate: { value: 0, min: 0, max: 360 },
+      complex: { value: 1, min: 1, max: 20, step: 1 },
+      morph: { value: 0., min: -1, max: 1 },
+    })
+  });
 
 
 
@@ -106,8 +105,8 @@ const SharpGradientRBg = (props: Mesh) => {
   const materialRef = useRef<THREE.ShaderMaterial>(null);
 
   // set palettes
-  const paletteLight = ["#0888B8", "#0870A8", "#f09878"].map((color) => new THREE.Color(color))
-  const paletteDark = [colors.color1, colors.color2, colors.color3].map((color) => new THREE.Color(color))
+  // const paletteLight = ["#0888B8", "#0870A8", "#f09878"].map((color) => new THREE.Color(color))
+  // const paletteDark = [colors.color1, colors.color2, colors.color3].map((color) => new THREE.Color(color))
 
   // animation
   useFrame(({ clock }) => {
@@ -131,10 +130,25 @@ const SharpGradientRBg = (props: Mesh) => {
     >
       <planeBufferGeometry args={[viewport.width, viewport.height, 1, 1]} />
       {/* @ts-ignore */}
-      <sharpGradientRMaterial key={SharpGradientRMaterial.key} ref={materialRef} uColor={[colors.color1, colors.color2, colors.color3, colors.color4].map((color) => new THREE.Color(color))} uResolution={new THREE.Vector2(viewport.width, viewport.height)} uLightness={advanced.lightness} uSpeed={animation.speed} uDensity={advanced.density} uCol={advanced.columns} />
-      <EffectComposer disableNormalPass multisampling={0}>
+      <sharpGradientRMaterial
+        key={SharpGradientRMaterial.key}
+        ref={materialRef}
+        uSpeed={animation.speed}
+        uTimeOffset={animation.timeOffset}
+        uLightness={color.lightness}
+        uPosition={new THREE.Vector2(shape.position.x, shape.position.y)}
+        uScale={new THREE.Vector2(shape.scaleX, shape.scaleY)}
+        // uRotate={shape.rotate}
+        uColor={[color.color1, color.color2, color.color3, color.color4].map((color) => new THREE.Color(color))}
+        uBgColor={color.bgColor}
+        uComplex={shape.complex}
+        uMorph={shape.morph}
+        uResolution={new THREE.Vector2(viewport.width, viewport.height)}
+
+      />
+      {/* <EffectComposer disableNormalPass multisampling={0}>
         {noisy && <Noise premultiply blendFunction={BlendFunction.ADD} />}
-      </EffectComposer>
+      </EffectComposer> */}
     </mesh>
   );
 };
